5b64f4863f46bd0a0d1fef0d79cc0bd05ffc699c
Mamun Ahmed <mamunahmedusa@gmail.com>
2025-03-24 17:17:51 +0600

    feat: added auth route

--- middleware/auth_middleware.go
+++ middleware/auth_middleware.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"go-crud-app/utils"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,7 +17,9 @@ func AuthMiddleware() gin.HandlerFunc {
 			return
 		}
 
-		claims, err := utils.ValidateToken(authHeader)
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+
+		claims, err := utils.ValidateToken(tokenString)
 		if err != nil {
 			c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
 			c.Abort()
@@ -36,3 +39,37 @@ func GetUserID(c *gin.Context) uint {
 
 	return userID.(uint)
 }
+
+// JWTAuthMiddleware verifies the JWT token in requests
+// func JWTAuthMiddleware() gin.HandlerFunc {
+// 	return func(c *gin.Context) {
+// 		authHeader := c.GetHeader("Authorization")
+// 		if authHeader == "" {
+// 			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header required"})
+// 			c.Abort()
+// 			return
+// 		}
+
+// 		tokenString := strings.Split(authHeader, "Bearer ")
+// 		if len(tokenString) != 2 {
+// 			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token format"})
+// 			c.Abort()
+// 			return
+// 		}
+
+// 		token, err := jwt.Parse(tokenString[1], func(token *jwt.Token) (interface{}, error) {
+// 			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+// 				return nil, jwt.ErrSignatureInvalid
+// 			}
+// 			return secretKey, nil
+// 		})
+
+// 		if err != nil || !token.Valid {
+// 			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+// 			c.Abort()
+// 			return
+// 		}
+
+// 		c.Next()
+// 	}
+// }
